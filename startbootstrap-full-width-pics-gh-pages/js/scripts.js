61e5e247bf00ce2d32a3d031bd84aefcced7d323
SRLoveys <147052904+SRLoveys@users.noreply.github.com>
2024-11-29 03:34:27 -0330

    Page navigation and about alerts

--- startbootstrap-full-width-pics-gh-pages/js/scripts.js
+++ startbootstrap-full-width-pics-gh-pages/js/scripts.js
@@ -5,3 +5,13 @@
 */
 // This file is intentionally blank
 // Use this file to add JavaScript to your project
+"use strict";
+
+const $ = (selector) => document.querySelector(selector);
+
+document.addEventListener("DOMContentLoaded", () => {
+    $("#aboutPage").addEventListener("click", evt => {
+        evt.preventDefault();
+        alert("Tester")
+    })
+})
